e9b59976d6da89f66eecbea0ac86cb3a8e9244e7
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-05-15 10:54:30 +0100

    Added split resize mappings
    Also .gitignore'd lazy-lock.json because if I want to control the version, I will specify it in the lazy plugin config.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,11 +2,18 @@ require "nvchad.mappings"
 
 -- add yours here
 
+print("atom-nvim: mappings")
+
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "config", ":e ~/AppData/Local/nvim/")
+
+map("n", "<m-h>", "<c-w>5<")
+map("n", "<m-j>", "<c-w>+")
+map("n", "<m-k>", "<c-w>-")
+map("n", "<m-l>", "<c-w>5>")
 
 map("n", "<leader>ra", function()
   vim.lsp.buf.rename()
