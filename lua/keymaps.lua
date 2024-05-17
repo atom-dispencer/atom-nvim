a70b5592c94a950b6d3f2519debc55fd0e269185
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-05-17 18:59:51 +0100

    Basic Oil setup

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -10,10 +10,14 @@ map("n", "<C-s>", ":update<cr>")
 
 -- Telescope
 local telescope = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
-vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
-vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
-vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
+map('n', '<leader>ff', telescope.find_files, {})
+map('n', '<leader>fg', telescope.live_grep, {})
+map('n', '<leader>fb', telescope.buffers, {})
+map('n', '<leader>fh', telescope.help_tags, {})
+
+
+-- Oil
+map("n", "<C-n>", "<CMD>Oil<CR>")
 
 
 -- Splits
--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -8,5 +8,8 @@ return {
     'stevearc/oil.nvim',
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("oil").setup()
+    end
   }
 }
