ccd67b3fe5ac037f73b8afb17feae899ac57a9ae
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-05-18 11:10:52 +0100

    Added Todo Comments

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -1,9 +1,10 @@
--- Configuration for the actual text editing bit
+-- Configuration for the text-editor area, but not LSP
 -- Includes snippets, completion and guides
 
 return {
 
 
+  -- TODO stuff
   -- 'Virtual text' indentation guides
   -- Currently just errors: utils.lua:428: attempt to call field 'iter' (a nil value)
   --{
@@ -11,4 +12,10 @@ return {
   --  main = "ibl",
   --  opts = {}
   --}
+  
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {}
+  }
 }
