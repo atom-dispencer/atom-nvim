05f66039c6500294b317cac8325eba82c2af62e3
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2024-07-11 11:13:10 +0100

    Disable Oil.nvim Ctrl-S

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -33,7 +33,11 @@ return {
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("oil").setup()
+      require("oil").setup({
+        keymaps = {
+          ["<C-s>"] = false
+        }
+      })
     end
   }
 }
