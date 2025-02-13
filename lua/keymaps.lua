ba6a3b0ecef2e2ddb512bc7605b9bd998a5b7fe6
Adam Spencer <46679475+atom-dispencer@users.noreply.github.com>
2025-02-13 15:54:14 +0000

    Remove FLOATING floaterminal keybinds

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,9 +5,7 @@ local map = vim.keymap.set
 -- Terminal
 --
 --
-local floaterminal = require("floaterminal")
 map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Return to normal mode from terminal mode" })
-map("n", "<C-x>", floaterminal.toggle_terminal, { desc = "Toggle a floating terminal window" })
 
 --
 --
